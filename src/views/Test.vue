0a0b1f41babded5ce4783acce470e828a604e886
Administrator <869646293@qq.com>
2019-11-22 11:52:35 +0800

    个人中心

---
+++ src/views/Test.vue
@@ -0,0 +1,14 @@
+<template>
+    <van-button color="linear-gradient(to right, #4bb0ff, #6149f6)">渐变色按钮</van-button>
+
+</template>
+
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
+
+<style scoped>
+
+</style>
