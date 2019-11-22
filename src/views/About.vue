83fb1970dd069811a9cbae703e05e54aad47bb57
Administrator <869646293@qq.com>
2019-11-22 11:47:08 +0800

    个人中心

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
+
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <van-button plain type="primary" round="true">朴素按钮</van-button>
+    <van-button color="linear-gradient(to right, #4bb0ff, #6149f6)">渐变色按钮</van-button>
+
+
+
   </div>
+
 </template>
+
+<script>
+
+
+</script>
