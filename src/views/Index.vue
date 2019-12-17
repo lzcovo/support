337e4027f947d3df61e50bed82cbe45c235ec30f
Administrator <869646293@qq.com>
2019-12-17 21:57:28 +0800

    个人中心

---
+++ src/views/Index.vue
@@ -0,0 +1,177 @@
+<template>
+
+
+    <div>
+        <van-row>
+            <router-link to="/Research">
+            <van-col span="22" style="margin-top: -10px"><van-search placeholder="请输入搜索关键词"  v-model="value" />
+            </van-col>
+            </router-link>
+            <router-link to="/Location">
+            <van-col span="2" ><van-icon name="location-o" size="17px"/>
+                <p style="margin-top: -5px;font-size: 5px">定 位</p>
+            </van-col>
+            </router-link>
+        </van-row>
+
+
+        <van-tabs v-model="active">
+            <van-tab title="今日发布">
+                <van-swipe :autoplay="3000" indicator-color="white">
+                    <van-swipe-item>
+                        <van-image
+                            width="500"
+                            height="200"
+                            src="https://img.yzcdn.cn/vant/apple-1.jpg"
+                        /></van-swipe-item>
+
+                    <van-swipe-item><van-image
+                            width="500"
+                            height="200"
+                            src="https://img.yzcdn.cn/vant/apple-2.jpg"
+                    /></van-swipe-item>
+
+                    <van-swipe-item><van-image
+
+                            width="500"
+                            height="200"
+                            src="https://img.yzcdn.cn/vant/apple-1.jpg"
+                    /></van-swipe-item>
+                    <van-swipe-item><van-image
+                            width="500"
+                            height="200"
+                            src="https://img.yzcdn.cn/vant/apple-2.jpg"
+                    /></van-swipe-item>
+                </van-swipe>
+                <van-cell-group >
+                    <van-row>
+                        <van-col span="6"><van-cell title="服务类型：" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="服务名称：" /></van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="6"><van-cell title="¥54：" style="color: red" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="已售7878" style="color: darkgray" /></van-col>
+                    </van-row>
+                </van-cell-group>
+
+                <div>
+                    <van-image
+                            width="100%"
+                            height="150"
+                            src="https://img.yzcdn.cn/vant/cat.jpeg"
+                    />
+                </div>
+                <van-cell-group >
+                    <van-row>
+                        <van-col span="6"><van-cell title="服务类型：" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="服务名称：" /></van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="6"><van-cell title="¥78：" style="color: red" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="已售6666" style="color: darkgray" /></van-col>
+                    </van-row>
+                </van-cell-group>
+                <div>
+                    <van-image
+                            width="100%"
+                            height="150"
+                            src="https://img.yzcdn.cn/vant/cat.jpeg"
+                    />
+                </div>
+                <van-cell-group >
+                    <van-row>
+                        <van-col span="6"><van-cell title="服务类型：" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="服务名称：" /></van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="6"><van-cell title="¥54：" style="color: red" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="已售9999" style="color: darkgray" /></van-col>
+                    </van-row>
+                </van-cell-group>
+
+
+
+            </van-tab>
+            <van-tab title="家政服务"><div>
+                <van-image
+                        width="100%"
+                        height="150"
+                        src="https://img.yzcdn.cn/vant/cat.jpeg"
+                />
+            </div>
+                <van-cell-group >
+                    <van-row>
+                        <van-col span="6"><van-cell title="服务类型：" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="服务名称：" /></van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="6"><van-cell title="¥54：" style="color: red" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="已售9999" style="color: darkgray" /></van-col>
+                    </van-row>
+                </van-cell-group>
+            </van-tab>
+            <van-tab title="民宿公寓"><div>
+                <van-image
+                        width="100%"
+                        height="150"
+                        src="https://img.yzcdn.cn/vant/cat.jpeg"
+                />
+            </div>
+                <van-cell-group >
+                    <van-row>
+                        <van-col span="6"><van-cell title="服务类型：" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="服务名称：" /></van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="6"><van-cell title="¥54：" style="color: red" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="已售9999" style="color: darkgray" /></van-col>
+                    </van-row>
+                </van-cell-group>
+            </van-tab>
+            <van-tab title="工具出租"><div>
+                <van-image
+                        width="100%"
+                        height="150"
+                        src="https://img.yzcdn.cn/vant/cat.jpeg"
+                />
+            </div>
+                <van-cell-group >
+                    <van-row>
+                        <van-col span="6"><van-cell title="服务类型：" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="服务名称：" /></van-col>
+                    </van-row>
+                    <van-row>
+                        <van-col span="6"><van-cell title="¥54：" style="color: red" /></van-col>
+
+                        <van-col offset="4" span="10"><van-cell title="已售9999" style="color: darkgray" /></van-col>
+                    </van-row>
+                </van-cell-group>
+            </van-tab>
+        </van-tabs>
+    </div>
+
+
+
+</template>
+
+<script>
+
+export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Research.vue
@@ -0,0 +1,58 @@
+<template>
+
+    <van-row>
+        <van-col style="width: 25%" >
+            <van-sidebar v-model="activeKey " @change="onChange" style="width: 100%">
+                <van-sidebar-item title="美食">
+
+                </van-sidebar-item>
+                <van-sidebar-item title="家政服务" />
+                <van-sidebar-item title="民宿公寓"/>
+                <van-sidebar-item title="生活服务" />
+                <van-sidebar-item title="居家百货" />
+                <van-sidebar-item title="洗护" />
+                <van-sidebar-item title="杂货" />
+                <van-sidebar-item title="饮食" />
+                <van-sidebar-item title="电器" />
+            </van-sidebar>
+        </van-col>
+        <van-col style="height: 100%;width: 70%"  span="7" >
+            <van-grid square style="text-align: center">
+                <van-grid-item
+                        v-for="value in 8"
+                        :key="value"
+                        icon="photo-o"
+                        text="文字"
+                />
+            </van-grid>
+        </van-col>
+    </van-row>
+
+
+
+
+</template>
+
+<script>
+    import { Notify } from 'vant';
+
+    export default {
+        name: "Research",
+        data() {
+            return {
+                activeKey: 0,
+
+
+            };
+        },
+        methods: {
+            onChange(index) {
+                Notify({ type: 'primary', message: index });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
